b2705a0afb4fa81b53fd163031286d62bcb67698
AislynnEdmiston <davidandaislynn@gmail.com>
2019-12-14 13:55:50 -0500

    get by id

--- routes/project.js
+++ routes/project.js
@@ -3,8 +3,17 @@ const projectModelRouter = require('express').Router()
 
 async function getAllProjects(req, res){
     try {
-        const projects = await projectModel.get()
+        const projects = await projectModel.find()
         res.json(projects)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+async function getProjectById(req, res){
+    try {
+        const project = await projectModel.get(id)
+        res.json(project[0])
     } catch (error) {
         res.status(500).json(error)
     }
@@ -13,5 +22,6 @@ async function getAllProjects(req, res){
 
 projectModelRouter
     .get('/', getAllProjects)
+    .get('/:id', getProjectById)
 
 module.exports = projectModelRouter
